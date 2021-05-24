916f7bc1cea8ada31b7415fc04fc9ce78146585f
newnag <newnagtl@gmail.com>
2021-05-24 16:56:46 +0700

    24/05/2021

--- js/job.js
+++ js/job.js
@@ -15,4 +15,36 @@ function timeline(){
   });
 }
 
+function step_form(that){
+  let step = that.getAttribute('data-step')
+  //console.log(document.querySelector('.form1 #name').getAttribute('id'))
+  if(step === "1"){
+    // check empty input
+    valid_input("empty",document.querySelector('.form1 #name'))
+
+  }
+}
+
+function valid_input(type,element){
+  if(type === "empty"){
+    if(element.length == 0 || element.value == ""){
+      Swal.fire(
+        'แจ้งเตือน',
+        'คุณกรอกข้อมูลไม่ครบ',
+        'warning'
+      ).then(()=>{
+        element.classList.add('valid-error')
+        element.placeholder = "กรุณากรอกข้อมูล"
+        zenscroll.center(element)
+      })
+    }
+    else{
+      element.classList.remove('valid-error')
+      element.placeholder = ""
+    }
+  }
+}
+
+/////////////////////////////////////////////////////////////////////
+//@ use function
 timeline()
